9ecc4102aa39da81928e116c7d71924694473579
luwl <luwl@getui.com>
2019-04-01 21:14:11 +0800

    feat(effective-front-end): add equal height layout demos ( with padding and table )

--- docs/.vuepress/components/demo-code.vue
+++ docs/.vuepress/components/demo-code.vue
@@ -43,7 +43,7 @@ export default {
       padding: 0;
       margin: 0;
       border-bottom: none;
-      font-size: 18px;
+      font-size: 16px;
     }
     small {
       font-size: 14px;
@@ -60,7 +60,7 @@ export default {
       0 1px 2px 0 rgba(232, 237, 250, 0.5);
     .code-content {
       display: flex;
-      justify-content: center;
+      /*justify-content: center;*/
       align-items: center;
       box-sizing: border-box;
       padding: 4%;
---
+++ docs/.vuepress/components/efe/demo-cols-same-height2.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="demo">
+    <div class="col col1">
+      <p class="title">苹果</p>
+      <p class="des">
+        苹果，是水果中的一种，是蔷薇科苹果亚科苹果属植物，其树为落叶乔木。苹果富含矿物质和维生素，是人们经常食用的水果之一。
+      </p>
+    </div>
+    <div class="col col2">
+      <p class="title">香蕉</p>
+      <p class="des">
+        香蕉（学名：Musa nana Lour.）芭蕉科芭蕉属植物，又指其果实，热带地区广泛种植。香蕉味香、富含营养，植株为大型草本，从根状茎发出，由叶鞘下部形成高3～6公尺(10～20尺)的假杆；叶长圆形至椭圆形，有的长达3～3.5公尺(10～11.5尺)，宽65公分(26寸)，10～20枚簇生茎顶。
+      </p>
+    </div>
+    <div class="col col3">
+      <p class="title">水蜜桃</p>
+      <p class="des">
+        水蜜桃（学名：Prunus persica 、meltingfleshed peach）：蔷薇科、桃属植物。南方品种群中肉质柔软多汁呈软溶质的一类品种。果实顶部平圆，熟后易剥皮，多粘核。 [1]
+      </p>
+    </div>
+    <div class="col col4">
+      <p class="title">菠萝</p>
+      <p class="des">
+        菠萝（学名：Ananas comosus），是热带水果之一。福建和台湾地区称之为旺梨或者旺来（ông-lâi），新马一带称为黄梨，大陆及香港称作菠萝。有70多个品种，岭南四大名果之一。
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'demo-cols-same-height2',
+  };
+</script>
+
+<style scoped lang="stylus">
+  .demo
+    display table
+    border-spacing 20px // <-- 设置间隔
+    word-break break-all
+  .col
+    display table-cell
+    width 1000px // <-- 设置很大的宽度，table 自动平分宽度
+    padding: 10px
+    border 1px solid #000
+    border-radius 5px
+  @media (max-width: 500px)
+    .demo
+      display block // <-- 可以覆盖原来的样式，实现响应式布局
+    .col
+      display block // <-- 可以覆盖原来的样式，实现响应式布局
+      width: 100%
+</style>

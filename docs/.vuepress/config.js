9eb90a2649a96841a96ef3ebee1e5fdf4cd01363
luwl <luwl@getui.com>
2019-04-03 15:34:26 +0800

    feat(effective-front-end): update logo & favicon; update title; add fontawesome; add override style

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,20 @@
 const router = require('./router/router.js');
-console.log(router, 11111)
 
 module.exports = {
   base: '/wendys-site/',
-  title: '晴天&Sunny',
+  title: 'Wendy\'s one peace',
+  head: [
+    ['link',
+      {
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.8.1/css/all.css',
+        crossorigin: 'anonymous',
+        integrity: 'sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf',
+      }
+    ]
+  ],
   // description: '晴天&Sunny',
-  userName: '晴天&Sunny',
+  userName: 'Wendy Lu',
   themeConfig: {
     displayAllHeaders: true,
     sidebar: 'auto',
@@ -18,4 +27,4 @@ module.exports = {
     // ],
     lastUpdated: 'Last Updated', // string | boolean
   }
-}
+};

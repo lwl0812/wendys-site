6e4a7d14d38b712e72788eeec69566164c343df5
luwl <luwl@getui.com>
2019-04-03 20:15:34 +0800

    feat(effective-front-end): add demo-attr(use attr to achieve cursor hover to display title)

---
+++ docs/.vuepress/components/efe/01/demo-attr.vue
@@ -0,0 +1,43 @@
+<template>
+  <p>
+    Hello
+    <span class="tip" data-title="Frontend Development">FED</span>
+  </p>
+</template>
+
+<script>
+  export default {
+    name: 'demo-attr',
+  };
+</script>
+
+<style scoped lang="stylus">
+.tip {
+  position: relative;
+  &:hover:before {
+    padding: 5px;
+    content: attr(data-title);
+    position: absolute;
+    left: 50%;
+    top: -230%;
+    transform: translateX(-50%);
+    white-space: nowrap;
+    border-radius: 5px;
+    background: #000;
+    color: #fff;
+  }
+  &:hover:after {
+    position: absolute;
+    top: -10px;
+    left: 50%;
+    transform: translateX(-50%);
+    content: '';
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 12px;
+    border-color: transparent;
+    border-top-color: #000;
+  }
+}
+</style>

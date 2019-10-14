efc4d3b6220eb52f53f6a9c048b880ac65170b0c
lwl0812 <1722072133@qq.com>
2019-10-14 14:03:19 +0800

    feat(面试): add currentColor demo

---
+++ docs/.vuepress/components/html-css/03.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="demo-currentcolor">
+    <p>鼠标 hover 观察效果</p>
+    <div>
+      <a href="javascript:void(0)" class="link"
+        >返回 <i class="el-icon-back icon"></i>（background-color 没有使用 currentColor）</a
+      >
+    </div>
+    <div>
+      <a href="javascript:void(0)" class="link"
+        >返回 <i class="el-icon-back icon1"></i>（background-color 使用 currentColor）</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="stylus">
+.demo-currentcolor {
+  .link {
+    color: #000;
+
+    &:hover {
+      color: #03A9F4;
+    }
+  }
+
+  .icon {
+    background-color: #000;
+  }
+
+  .icon1 {
+    background-color: currentColor;
+  }
+}
+</style>
